892ce7f2e63a9662e0ed4cc095c9a1f646def4dd
Sarmad <sarmadnaroo.bsiietkhpf20@iba-suk.edu.pk>
2024-08-28 21:09:33 +0500

    success bool added

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -8,14 +8,14 @@ exports.signup = async (req, res) => {
     
     try {
         const existingUser = await User.findOne({ email });
-        if (existingUser) return res.status(400).json({ message: 'User already exists' });
+        if (existingUser) return res.status(400).json({ success: false, message: 'User already exists' });
 
         const newUser = new User({ email, password });
         await newUser.save();
 
-        res.status(201).json({ message: 'User created successfully' });
+        res.status(201).json({ success: true, message: 'User created successfully' });
     } catch (err) {
-        res.status(500).json({ message: 'Server error', error: err.message });
+        res.status(500).json({ success: false, message: 'Server error', error: err.message });
     }
 };
 
@@ -24,25 +24,26 @@ exports.login = async (req, res) => {
     
     try {
         const user = await User.findOne({ email });
-        if (!user) return res.status(400).json({ message: 'Invalid credentials' });
+        if (!user) return res.status(400).json({ success: false, message: 'Invalid credentials' });
 
         const isMatch = await user.comparePassword(password);
-        if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
+        if (!isMatch) return res.status(400).json({ success: false, message: 'Invalid credentials' });
 
         const token = jwt.sign({ userId: user._id }, config.jwtSecret, { expiresIn: '1h' });
 
-         // Determine IP address
-         const ip = req.headers['x-forwarded-for'] || req.ip;
+        // Determine IP address
+        const ip = req.headers['x-forwarded-for'] || req.ip;
 
-         // Store login information
-         const loginInfo = new LoginInfo({
-             user: user._id,
-             ip: ip
-         });
-         await loginInfo.save();
+        // Store login information
+        const loginInfo = new LoginInfo({
+            user: user._id,
+            ip: ip
+        });
+        await loginInfo.save();
 
-        res.status(200).json({ email: user.email, token });
+        res.status(200).json({ success: true, email: user.email, token });
     } catch (err) {
-        res.status(500).json({ message: 'Server error', error: err.message });
+        res.status(500).json({ success: false, message: 'Server error', error: err.message });
     }
 };
+
