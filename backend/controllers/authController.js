59c3eae9355965fe8828058c48a7cf51d7ae6eef
Sarmad <sarmadnaroo.bsiietkhpf20@iba-suk.edu.pk>
2024-08-30 00:14:53 +0500

    username in signup added

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,13 +4,13 @@ const config = require('../config/config');
 const LoginInfo = require('../models/LoginInfo');
 
 exports.signup = async (req, res) => {
-    const { email, password } = req.body;
+    const { username, email, password } = req.body;
     
     try {
         const existingUser = await User.findOne({ email });
         if (existingUser) return res.status(400).json({ success: false, message: 'User already exists' });
 
-        const newUser = new User({ email, password });
+        const newUser = new User({ username, email, password });
         await newUser.save();
 
         res.status(201).json({ success: true, message: 'User created successfully' });
